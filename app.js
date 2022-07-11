deb74ebae4e8a9a0e8e1330ff501c86fd24304cb
ForLunch01 <crazylunch2019@gmail.com>
2022-07-11 18:48:36 +0900

    init project

---
+++ app.js
@@ -0,0 +1,20 @@
+let age = 32;
+let userName = "Lunch";
+let hobbies = ["Reading", "Game", "Music"];
+let job = {
+  title: "Developer",
+  place: "New York",
+  salary: 50000,
+};
+
+let longText = `it
+can use
+new line text`
+
+let adultYears = age - 18;
+
+function calAdultYears(age) {
+    newAge = age-18
+    return newAge
+}
+
---
+++ operations.js
@@ -0,0 +1,5 @@
+console.log(10 + 4);
+
+let usersName = 'Lunchchchcchch'
+console.log("usersName:", usersName.length)
+console.log(usersName.toUpperCase())
