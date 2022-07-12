4657189857e11c76d5ecd077854642812cce38e7
ForLunch01 <crazylunch2019@gmail.com>
2022-07-12 14:21:48 +0900

    js module3 init

--- module_2/app.js
+++ module_2/app.js
@@ -7,7 +7,7 @@
 let anchorElement = document.getElementById('external-link');
 anchorElement.href = "https://google.com"
 
-anchorElement = document.querySelector('#external-id');
+anchorElement = document.querySelector('#external-link');
 anchorElement.href = "https://google.com"
 
 anchorElement = document.querySelector('p a');
@@ -15,3 +15,39 @@ anchorElement.href = "https://google.com"
 
 // anchorElement = document.querySelectorAll('p a');
 // anchorElement.href = "https://google.com"
+
+
+// Element 추가
+// 1. Create the new element
+
+let newAnchorElement = document.createElement('a');
+newAnchorElement.href = 'https://google.com';
+newAnchorElement.textContent = 'This leads to  Google!';
+
+// 2. Get access to the parent element that should hold the new element
+
+let firstParagraph = document.querySelector('p');
+
+// 3. Insert the new element into the parent element content
+
+firstParagraph.append(newAnchorElement);
+
+
+// Element 삭제
+
+let firstH1Element = document.querySelector('h1');
+// firstH1Element.remove();
+
+firstH1Element.parentElement.removeChild(firstH1Element); // for older browser
+
+
+
+// MOVE ELEMENTS
+
+firstParagraph.parentElement.append(firstParagraph);
+
+// innerHTML
+
+console.log(firstParagraph.innerHTML);
+
+firstParagraph.innerHTML = 'Hi! This is <strong>important</strong>.'
