8dc42e9f846c4e78157aa8022abc462fa72eb677
ForLunch01 <crazylunch2019@gmail.com>
2022-07-14 16:48:27 +0900

    tictactoe finished

--- module_6/scripts/config.js
+++ module_6/scripts/config.js
@@ -37,11 +37,4 @@ function savePlayerConfig(event) {
   closePlayerConfig();
 }
 
-function startNewGame() {
-    if (players[0].name === "" || players[1].name === "") {
-      alert('Please set custom player names for both players!');
-      return;
-    }
-    gameAreaElement.style.display = "block";
-  }
-  
+
