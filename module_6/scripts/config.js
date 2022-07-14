4349a174c8661df5e87e0a963cd9ce840747979a
ForLunch01 <crazylunch2019@gmail.com>
2022-07-14 09:11:49 +0900

    js moudle update

--- module_6/scripts/config.js
+++ module_6/scripts/config.js
@@ -1,4 +1,6 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+    editedPlayer = +event.target.dataset.playerid; // + means change to num
+
     playerConfigOverlayElement.style.display = 'block';
     backdropElement.style.display = 'block';
 }
@@ -6,11 +8,19 @@ function openPlayerConfig() {
 function closePlayerConfig() {
     playerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
+
+    formElement.firstElementChild.classList.remove('error');
+    errorsOutputElement.textContent = '';
 }
 
 function savePlayerConfig(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
-    const enteredPlayername = formData.get('playername');
+    const enteredPlayername = formData.get('playername').trim();
     
+    if (!enteredPlayername) { // JS에서 공백 문자는 false로 취급됨
+        event.target.firstElementChild.classList.add('error');
+        errorsOutputElement.textContent = 'Please enter a valid name!';
+        return;
+    }
 }
