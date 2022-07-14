25a9a3ebd4a1c3223d76ff0497d8a5197d8a8c67
ForLunch01 <crazylunch2019@gmail.com>
2022-07-14 11:28:46 +0900

    add basic game logic

--- module_6/scripts/config.js
+++ module_6/scripts/config.js
@@ -1,26 +1,47 @@
 function openPlayerConfig(event) {
-    editedPlayer = +event.target.dataset.playerid; // + means change to num
+  editedPlayer = +event.target.dataset.playerid; // + means change to num
 
-    playerConfigOverlayElement.style.display = 'block';
-    backdropElement.style.display = 'block';
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
 }
 
 function closePlayerConfig() {
-    playerConfigOverlayElement.style.display = 'none';
-    backdropElement.style.display = 'none';
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
 
-    formElement.firstElementChild.classList.remove('error');
-    errorsOutputElement.textContent = '';
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+
+  formElement.firstElementChild.lastElementChild.value = "";
 }
 
 function savePlayerConfig(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const enteredPlayername = formData.get('playername').trim();
-    
-    if (!enteredPlayername) { // JS에서 공백 문자는 false로 취급됨
-        event.target.firstElementChild.classList.add('error');
-        errorsOutputElement.textContent = 'Please enter a valid name!';
-        return;
-    }
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get("playername").trim();
+
+  if (!enteredPlayername) {
+    // JS에서 공백 문자는 false로 취급됨
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
+
+  const updatedPlayerDataElement = document.getElementById(
+    "player-" + editedPlayer + "-data"
+  );
+  updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+
+  players[editedPlayer - 1].name = enteredPlayername;
+
+  closePlayerConfig();
 }
+
+function startNewGame() {
+    if (players[0].name === "" || players[1].name === "") {
+      alert('Please set custom player names for both players!');
+      return;
+    }
+    gameAreaElement.style.display = "block";
+  }
+  
